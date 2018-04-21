d5334f4b16dc7f635eec44173b84238db81f48e6
Jared Allard <jaredallard@outlook.com>
2018-04-20 18:33:19 -0700

    feat: rewrite

--- index.js
+++ index.js
@@ -1,237 +1,180 @@
-
 /**
- * Music Bot
- *
+ * Bot attempt 2
+ * 
  * @author Jared Allard <jaredallard@outlook.com>
  * @license MIT
- * @version 1
+ * @version 2
  */
-'use strict';
 
-const debug = require('debug')('bot')
-const Shino = require('shinojs')
-const request = require('request-promise')
+const TelegramBot = require('node-telegram-bot-api')
+const MPD = require('./lib/mpd')
+const debug = require('debug')('bot:main')
 
-const API_URL = process.env.API_URL || 'http://127.0.0.1:8100'
+const mpd = new MPD()
+const bot = new TelegramBot(require('./config/config.json').telegram, {
+  polling: true
+})
 
-let app;
+// O(3)
+const database = {
+  idDataMap: {}, // file -> data -> id
+  dataMapId: {}, // data -> file -> id
+  data: [],
+  lastId: 0
+}
+
+const sendMessage = (id, text) => {
+  return bot.sendMessage(id, text, {
+    parse_mode: 'Markdown'
+  })
+}
 
 /**
- * Wait for x time
- * @param  {Number} [int=5000] [description]
- * @return {Promise}           .then on finish
+ * Add song to db or return it's current ID.
+ * @param {String} file file name
  */
-const wait  = (int = 1300) => {
-  return new Promise(resolv => {
-    setTimeout(resolv, int)
-  })
-}
-
-const init = async () => {
-
-  app = new Shino(require('./config/config.json'), 'telegram')
-
-  debug('init', 'init shino')
-  await app.init()
-
-  /**
-   * Erro wrapper
-   * @param {Object} msg - {ShinoJS.message}
-   */
-  const error = msg => msg.reply("I'm sorry, I couldn't communicate with the server 😢")
-
-  /**
-   * Queue a song to play next
-   * @param {String} path - File path
-   * @param {ShinoJS.Message} msg  - Message Object
-   */
-  const queueSong = async (song, msg) => {
-    const { title, artist, path } = song
-    
-    const res = await request.post({
-      uri: `${API_URL}/v1/player/queue`,
-      body: {
-        file: path
-      },
-      json: true
-    })
-
-    if(!res.success) return error(msg)
-
-    await msg.reply(`OK! I queued ${artist} - ${title} for you!`)
+const addSong = file => {
+  // we already stored the id
+  const existingId = database.data.indexOf(file)
+  if(existingId !== -1) {
+    const id = database.dataMapId[existingId]
+    return id
   }
 
-  app.register('nowplaying', async (adapter, msg) => {
-    const res = await request.get({
-      uri: `${API_URL}/v1/player/status`,
-      json: true
-    })
-    if(!res.success) return error(msg)
+  const newId = ++database.lastId
+  const songPos = database.data.push(file)-1
 
-    const data = res.data
-
-    debug('now-playing', data)
-    msg.reply(`Now playing: ${data.title} by ${data.artist} 🎵`)
-  })
-
-  app.register('queue_next', async (adapter, msg) => {
-    const res = await request.get({
-      uri: `${API_URL}/v1/player/queue?limit=5`,
-      json: true
-    })
-    if(!res.success) return error(msg)
-    const data = res.data
-
-    debug('queue', data)
-
-    const text = [
-      "Here's what's currently next up:"
-    ]
-    data.forEach((song, index) => {
-      text.push(` ${index+1}. ${song.artist} - ${song.title}`)
-    })
-
-    msg.reply(text.join('\n'))
-  })
-
-  // Hacky attempt at getting unrecognized output to
-  // queue a song / search for it.
-  app.register('search', async (adapter, msg, details) => {
-    // TODO: need to implement in shino
-    const song = msg.text
-
-    debug('searching for', song)
-    const res = await request.post({
-      uri: `${API_URL}/v1/search`,
-      body: {
-        query: song
-      },
-      json: true
-    })
-    if(!res.success) return error(msg)
-    const data = res.data
-
-    console.log(msg.analysis)
-
-    if(!data[0]) {
-      details.setContext('')
-      return msg.reply(`I'm sorry, but I couldn't find a song by '${song}'.`)
-    }
-
-    if(data.length === 1) {
-      details.setContext('')
-      return queueSong(data[0], msg)
-    }
-
-    if(data.length > 10) return msg.reply("I'm sorry... but I found too many songs with that name. Please be more specific!")
-
-    const map = {}
-    const text = [
-      "I found these songs! Please give me a number if you'd like to listen to it now!",
-      ""
-    ]
-    data.forEach((song, index) => {
-      const num = index + 1
-      map[num] = song
-      text.push(` ${num}. ${song.artist} - ${song.title}`)
-    })
-
-    text.push('\nOr say anything else to cancel!')
-
-    details.stash(map)
-    msg.reply(text.join('\n'))
-  })
-
-  app.register('search:next', async (adapter, msg, details) => {
-    const stash = details.getStash() || {}
-
-    debug('stash', stash)
-
-    if(!stash[msg.text]) {
-      return msg.reply("OK! I won't play any of these.")
-    }
-
-    await queueSong(stash[msg.text], msg)
-  })
-
-  app.register('start', async (adapater, msg) => {
-    const lines = [
-      "Hello! I'm Asada, you can talk to me to interact with shinoradio.",
-      "",
-      "To *search* for or *play* a song, just type it!",
-      "Ask me \"*what's playing*\" to find out the current song,",
-      "or even \"*what's next*\" to get a list of songs coming up next.",
-      "",
-      "If you have any questions, message [@jaredallard](telegram://jaredallard).",
-      "🎵 http://music.tritonjs.com:8000/stream 🎵"
-    ]
-    msg.reply(lines.join('\n'))
-  })
-
-  app.register('like', async (adapater, msg) => {
-    msg.reply("I'm glad you like it!~ 🎵")
-  })
-
-  app.on('dm', {
-    version: 2,
-    classifiers: [
-      'playing',
-      ['song', 'this'],
-      ['what\'s', 'playing']
-    ],
-    action: 'nowplaying'
-  })
-
-  app.on('dm', {
-    version: 2,
-    classifiers: [
-      'queue',
-      'what\'s next'
-    ],
-    action: 'queue_next'
-  })
-
-  // /start handler
-  app.on('dm', {
-    version: 2,
-    address: 'start',
-    text: '/start',
-    action: 'start'
-  })
-
-  app.on('dm', {
-    version: 2,
-    address: 'like',
-    text: '👍',
-    action: 'like'
-  })
-
-  // search handler
-  app.on('dm', {
-    version: 2,
-    address: 'unknown',
-    classifiers: [
-      'search'
-    ],
-    default: 'drop',
-    action: 'search',
-    children: [
-      {
-        version: 2,
-        address: 'next',
-        action: 'search:next'
-      }
-    ]
-  })
-
-  app.done()
+  // Map the values.
+  database.idDataMap[newId] = songPos
+  database.dataMapId[songPos] = newId
+  
+  return newId
 }
 
+const getFile = id => {
+  const pointer = database.idDataMap[id]
+  if(!pointer) return false
 
-// Don't just get a "oh fuck a promise failed" message.
-process.on('unhandledRejection', reason => {
-  console.log('Unhandled Promise Rejection', reason)
-});
+  const data = database.data[pointer]
+  if(!data) return false
 
-// start the application
-init()
+  return data;
+}
+
+// Commands
+bot.onText(/^\/start/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const lines = [
+    "Hello! I'm Asada, you can talk to me to interact with shinoradio.",
+    "",
+    "To *search* for or *play* a song, just type it!",
+    "Ask me \"*what's playing*\" to find out the current song,",
+    "or even \"*what's next*\" to get a list of songs coming up next.",
+    "",
+    "If you have any questions, message [@jaredallard](telegram://jaredallard).",
+    "🎵 http://music.tritonjs.com:8000/stream 🎵"
+  ]
+
+  sendMessage(chatId, lines.join('\n'))
+})
+
+// search <type> <query>
+const search = async (msg, match) => {
+  const chatId = msg.chat.id;
+  const type = match[1]
+  const query = match[2].replace(/^\s+/g, '')
+
+  sendMessage(chatId, `Searching for ${type} ${query}`)
+
+  try {
+    const res = await mpd.find(query)
+    if(!res.success) throw new Error('I failed to talk to the server... oops.')
+    if(!res.data[0]) throw new Error("I couldn't find anything...")
+
+    const lines = [
+      "I found these!",
+      ""
+    ]
+
+    res.data.forEach(song => {
+      const id = addSong(song.path)
+      lines.push(` *${id}.* ${song.artist} - ${song.title}`)
+    })
+
+    lines.push("\nAdd any of these to the queue by ID with *play #ID*")
+
+    return sendMessage(chatId, lines.join('\n'))
+  } catch(err) {
+    return sendMessage(chatId, err.message)
+  }
+}
+
+const play = async (msg, match) => {
+  const chatId = msg.chat.id;
+  const id = match[1].replace('#', '')
+
+  const file = getFile(id)
+  if(!file) return sendMessage(chatId, "I couldn't find that ID...")
+
+  const res = await mpd.play(file)
+  debug('player:queue', res)
+  if(!res.success) return sendMessage(chatId, "I couldn't queue that right now, try again~? 🎵")
+  sendMessage(chatId, `Added '${file}' to the queue!`)
+}
+
+const nowPlaying = async (msg) => {
+  const chatId = msg.chat.id
+
+  const res = await mpd.playing()
+  debug('player', res)
+
+  sendMessage(chatId, `Now Playing: *${res.data.artist} - 「${res.data.title}」*`)
+}
+
+const queue = async (msg) => {
+  const chatId = msg.chat.id
+  
+  const res = await mpd.queue()
+  debug('player:queue', res)
+  if(!res.success) return sendMessage(chatId, "I failed to retrieve the queue....")
+  const data = res.data
+
+  debug('queue', data)
+
+  const text = [
+    "Here's what's currently next up:",
+    ""
+  ]
+  data.forEach((song, index) => {
+    const id = addSong(song.path)
+    text.push(` *${id}.* ${song.artist} - ${song.title}`)
+  })
+
+  sendMessage(chatId, text.join('\n'))
+}
+
+// now playing
+bot.onText(/^what(.?)s playing\??/, nowPlaying)
+bot.onText(/^(\/?)playing/, nowPlaying)
+
+// queue
+bot.onText(/^(\/?)queue(d?)/, queue)
+bot.onText(/^what(.?)s (next|queued)\??/, queue)
+
+// search
+bot.onText(/^\/search (\w+)?(.*)/, async (msg, match) => {
+  const chatId = msg.chat.id
+  const type = match[1]
+  const query = match[2]
+
+  if(!query) {
+    return sendMessage(chatId, "Oops, you forgot to tell me the type! Try: /search artist RADWIMPS")
+  }
+
+  await search(msg, match)
+})
+bot.onText(/^(?:search|find) (?:for)?(?: ?the)? (\w+) (.+)/, search)
+
+// play
+bot.onText(/^play (#?\d+)/, play);
+bot.onText(/^\/play (#?\d+)/, play)
